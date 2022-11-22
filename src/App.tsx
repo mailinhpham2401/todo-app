f82d76db970e220a00c1a2b00fd2711529c23a2b
LAPTOP-E5M1J98K\maili <mailinhh2401@gmail.com>
2022-11-22 19:50:32 +0100

    comments deleted and 4 space indents updated

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import InputForm from "./components/InputForm";
 function App() {
   const [todoList, setTodoList] = useState<TodoList>([]);
 
-  /*  function setItwm(item) {
-    setTodoList([item])
-  } */
-
   return (
     <div className="App">
       <InputForm setTodoList={setTodoList} todoList={todoList} />
-
       {todoList.map((todoItem, idx) => {
         return (
-          <div key={idx} style={{ margin: "10px", backgroundColor: "red" }}>
+          <div key={idx} 
+          style={{ margin: "10px",
+          backgroundColor: "red" }}>
             <p>{todoItem.task}</p>
             <p>{todoItem.done ? "done" : "not done"}</p>
+            <button>delete</button>
           </div>
         );
       })}
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -2,43 +2,43 @@ import React, { useState } from "react";
 import { TodoItem, TodoList } from "../interfaces/todo";
 
 export interface HandleInput {
-  (event: React.FormEvent<HTMLInputElement>): void;
+	(event: React.FormEvent<HTMLInputElement>): void;
 }
 
 interface InputFormProps {
-  setTodoList: React.Dispatch<TodoList>;
-  todoList: TodoList;
+	setTodoList: React.Dispatch<TodoList>;
+	todoList: TodoList;
 }
 
 export default function InputForm({ setTodoList, todoList }: InputFormProps) {
-  const [inputText, setInputText] = useState<string>("");
+	const [inputText, setInputText] = useState<string>("");
 
-  const handleInput: HandleInput = (event) => {
-    const textTarget = event.target;
-    const text = (textTarget as HTMLInputElement).value;
-    setInputText(text);
-  };
+	const handleInput: HandleInput = (event) => {
+		const textTarget = event.target;
+		const text = (textTarget as HTMLInputElement).value;
+		setInputText(text);
+	};
 
-  const submitTodo = (event: React.FormEvent) => {
-    event.preventDefault();
-    let updatedList = [...todoList];
-    const item: TodoItem = {
-      task: inputText,
-      time: new Date(),
-      done: false,
-      id: Math.random(),
-    };
-    updatedList.push(item);
-    setTodoList(updatedList);
-  };
+	const submitTodo = (event: React.FormEvent) => {
+		event.preventDefault();
+		let updatedList = [...todoList];
+		const item: TodoItem = {
+			task: inputText,
+			time: new Date(),
+			done: false,
+			id: Math.random(),
+		};
+		updatedList.push(item);
+		setTodoList(updatedList);
+	};
 
-  return (
-    <div>
-      {inputText}
-      <form onSubmit={submitTodo}>
-        <input type="text" name="task" onChange={handleInput} />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			{inputText}
+			<form onSubmit={submitTodo}>
+				<input type="text" name="task" onChange={handleInput} />
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 }
